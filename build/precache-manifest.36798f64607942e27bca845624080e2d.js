f46830f76552dc3fdeeef2cdd22075f71e6275b2
seguthart <seguthart@gmail.com>
2020-03-29 14:50:51 -0400

    added build dir to source control

---
+++ build/precache-manifest.36798f64607942e27bca845624080e2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fe5dd96de4d9e637145f5631822ab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1607c1ae90e42e49e24",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2b722f1c502aa055ae9",
+    "url": "/static/js/2.665367ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.665367ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1607c1ae90e42e49e24",
+    "url": "/static/js/main.a53c474f.chunk.js"
+  },
+  {
+    "revision": "125053e55612d1ddb60c",
+    "url": "/static/js/runtime-main.1330d221.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
